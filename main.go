2081f1d59819d3ca4e0c57a8359e80d79d2bc823
Lorenzo Fontana <fontanalorenzo@me.com>
2016-05-04 01:19:55 +0200

    Docker machine router as a configuration command

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/fntlnz/docker-machine-router/events"
+	"github.com/fntlnz/docker-machine-router/host"
 	"github.com/fntlnz/docker-machine-router/machine"
+	"github.com/fntlnz/docker-machine-router/network"
 	docker "github.com/fsouza/go-dockerclient"
 )
 
-const IP_CLASS = "10.0.0"
+var (
+	cidr  string
+	debug bool
+)
+
+const (
+	BANNER = `
+DOCKER MACHINE ROUTER
+This tool allows you to reach the container's internal ip addresses from the host by routing the OS X host traffic trough the Docker Machine VM.
+(c) 2016 Lorenzo Fontana
+version: %s
+`
+	version = "0.2.0"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, BANNER, version)
+	flag.PrintDefaults()
+}
+
+func init() {
+	flag.StringVar(&cidr, "cidr", "10.18.0.0/16", "Used to identify the IP address range that can be allocated by docker-machine-router")
+	flag.BoolVar(&debug, "debug", false, "Start in debug mode, provides a lot more information")
+	flag.Usage = usage
+	flag.Parse()
+}
 
 func main() {
 	logger := logrus.New()
@@ -18,48 +46,30 @@ func main() {
 		logger.Fatal("docker-machine-router must be run as root")
 	}
 
-	defer func() {
-		if r := recover(); r != nil {
-			logger.Fatal("An error occurred: ", r)
-		}
-	}()
-
 	client, err := docker.NewClientFromEnv()
 
 	if err != nil {
-		logger.Fatal("An error occurred connecting to the Docker daemon")
+		logger.Fatal("An error occurred contacting the Docker daemon: ", err.Error())
 	}
 
 	err = client.Ping()
 
 	if err != nil {
-		logger.Fatalf("Can't reach the docker daemon")
+		logger.Fatal("Can't reach the docker daemon")
 	}
 
-	mc, err := machine.NewMachineCmdExecutor(client)
+	_, err = network.CreateNetwork(client, cidr)
 
 	if err != nil {
-		logger.Fatal("An error occurred creating the docker-machine command executor: %s", err.Error())
+		logger.Fatal("An error occurred while creating the Docker Network: ", err.Error())
 	}
 
-	logger.Info("GC IPs on docker machine")
-	err = mc.DeAllocateIPClass(IP_CLASS)
+	machineIP, err := machine.MachineIPFromClient(client)
 
 	if err != nil {
-		logger.Warn("Problem occurred GC IPs on docker machine")
+		logger.Fatal("An error occurred while trying to obtain the Docker Machine ip address: ", err.Error())
 	}
 
-	logger.Info("Preallocating IPs on docker machine")
-	err = mc.PreAllocateIPClass(IP_CLASS)
-
-	if err != nil {
-		logger.Warnf("Problem occurred preallocating IPs on docker machine")
-	}
-
-	listener := events.NewDockerListener(client, logger)
-	err = listener.Listen()
-
-	if err != nil {
-		logger.Fatal("Error occurred attaching to the Docker daemon event stream: ", err.Error())
-	}
+	host.DeallocateHostIp(machineIP.String(), cidr)
+	host.AllocateHostIp(machineIP.String(), cidr)
 }
---
+++ network/network.go
@@ -0,0 +1,40 @@
+package network
+
+import docker "github.com/fsouza/go-dockerclient"
+
+const NETWORK_NAME = "dmr"
+
+func CreateNetwork(client *docker.Client, cidr string) (*docker.Network, error) {
+	ipamCfg := []docker.IPAMConfig{
+		docker.IPAMConfig{
+			Subnet: cidr,
+		},
+	}
+	ipamOpts := docker.IPAMOptions{
+		"default",
+		ipamCfg,
+	}
+
+	netOpts := docker.CreateNetworkOptions{
+		NETWORK_NAME,
+		false,
+		"bridge",
+		ipamOpts,
+		nil,
+		false,
+		false,
+	}
+
+	n, err := client.NetworkInfo(NETWORK_NAME)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if n != nil {
+		client.RemoveNetwork(n.ID)
+	}
+
+	return client.CreateNetwork(netOpts)
+
+}
